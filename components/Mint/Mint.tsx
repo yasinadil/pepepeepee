38d6ee3c495397679e0e07f02f32ef256d1ef914
yasinadil <adilyasin205@gmail.com>
2023-05-06 20:01:42 +0500

    mint ui

--- components/Mint/Mint.tsx
+++ components/Mint/Mint.tsx
@@ -1,22 +1,53 @@
 "use client";
 import Image from "next/image";
 import { ToastContainer, toast } from "react-toastify";
-// import ConnectButton from "../../public/assets/Button_Connect-Wallet.webp";
-import { ThirdwebProvider, metamaskWallet } from "@thirdweb-dev/react";
-import { ConnectWallet } from "@thirdweb-dev/react";
-import { Web3Button } from "@thirdweb-dev/react";
+import "@rainbow-me/rainbowkit/styles.css";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import IndeterminateCheckBoxIcon from "@mui/icons-material/IndeterminateCheckBox";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+
+const { chains, provider } = configureChains(
+  [polygonMumbai],
+  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_API_KEY! })]
+);
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Suggested",
+    wallets: [metaMaskWallet({ chains })],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
 
 export default function Mint() {
+  const [count, setCount] = useState(0);
+
+  const handleAddition = () => {
+    if (count < 8) {
+      setCount(() => count + 1);
+    }
+  };
+
+  const handleSubtraction = () => {
+    if (count > 0) {
+      setCount(() => count - 1);
+    }
+  };
   return (
-    <ThirdwebProvider
-      supportedWallets={[metamaskWallet()]}
-      autoConnect={true}
-      activeChain="mumbai"
-      sdkOptions={{
-        alchemyApiKey: process.env.NEXT_PUBLIC_API_KEY!, // your Alchemy API key
-      }}
-    >
+    <>
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -29,76 +60,128 @@ export default function Mint() {
         pauseOnHover
         theme="light"
       />
+
       <div id="subtitles" className="mt-4 text-center">
         <div id="connect" className="mt-5 flex justify-center">
-          {/* <button>
-            <Image
-              alt="Connect wallet"
-              src={ConnectButton}
-              width="225"
-              height="85"
-              decoding="async"
-              data-nimg="1"
-              loading="lazy"
-              style={{ color: "transparent" }}
-            />
-          </button> */}
-          <ConnectWallet
-            className="connect-btn"
-            btnTitle="CONNECT WALLET"
-            theme="light"
-          />
+          <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider chains={chains}>
+              <ConnectButton.Custom>
+                {({
+                  account,
+                  chain,
+                  openAccountModal,
+                  openChainModal,
+                  openConnectModal,
+                  authenticationStatus,
+                  mounted,
+                }) => {
+                  // Note: If your app doesn't use authentication, you
+                  // can remove all 'authenticationStatus' checks
+                  const ready = mounted && authenticationStatus !== "loading";
+                  const connected =
+                    ready &&
+                    account &&
+                    chain &&
+                    (!authenticationStatus ||
+                      authenticationStatus === "authenticated");
+                  return (
+                    <div
+                      {...(!ready && {
+                        "aria-hidden": true,
+                        style: {
+                          opacity: 0,
+                          pointerEvents: "none",
+                          userSelect: "none",
+                        },
+                      })}
+                    >
+                      {(() => {
+                        if (!connected) {
+                          return (
+                            <button
+                              className="text-5xl bg-[#7EB14A] px-4 py-1 rounded-lg"
+                              onClick={openConnectModal}
+                              type="button"
+                            >
+                              Connect Wallet
+                            </button>
+                          );
+                        }
+                        if (chain.unsupported) {
+                          return (
+                            <button
+                              className="text-5xl bg-[#7EB14A] px-4 py-1 rounded-lg"
+                              onClick={openChainModal}
+                              type="button"
+                            >
+                              Switch Network
+                            </button>
+                          );
+                        }
+                        return (
+                          <div>
+                            <div className="flex gap-x-4 justify-center items-center">
+                              <IndeterminateCheckBoxIcon
+                                onClick={handleSubtraction}
+                                className="cursor-pointer text-[#7EB14A]"
+                                fontSize="large"
+                              />
+                              <h1 className="text-5xl w-12">{count}</h1>
+                              <AddBoxIcon
+                                onClick={handleAddition}
+                                className="cursor-pointer text-[#7EB14A]"
+                                fontSize="large"
+                              />
+                            </div>
+                            <div className="text-4xl md:text-5xl">
+                              Price: {0.005 * count} ETH
+                            </div>
+                            <button className="text-4xl md:text-5xl bg-[#7EB14A] px-4 py-1 rounded-lg my-4">
+                              MINT
+                            </button>
+                            <div className="flex justify-center mt-5">
+                              <button
+                                onClick={openAccountModal}
+                                style={{
+                                  display: "flex",
+                                  alignItems: "center",
+                                }}
+                                type="button"
+                              >
+                                {chain.hasIcon && (
+                                  <div
+                                    style={{
+                                      background: chain.iconBackground,
+                                      width: 24,
+                                      height: 24,
+                                      borderRadius: 999,
+                                      overflow: "hidden",
+                                      marginRight: 4,
+                                    }}
+                                  >
+                                    {chain.iconUrl && (
+                                      <img
+                                        alt={chain.name ?? "Chain icon"}
+                                        src={chain.iconUrl}
+                                        style={{ width: 24, height: 24 }}
+                                      />
+                                    )}
+                                  </div>
+                                )}
+                                {/* {chain.name} */}
+                              </button>
+                            </div>
+                          </div>
+                        );
+                      })()}
+                    </div>
+                  );
+                }}
+              </ConnectButton.Custom>
+            </RainbowKitProvider>
+          </WagmiConfig>
         </div>
       </div>
-    </ThirdwebProvider>
+    </>
   );
-}
-
-{
-  /* <Web3Button
-                  className="bg-[#e7d17a] text-black py-2 hover:bg-red-600"
-                  contractAddress={contractAddress}
-                  contractAbi={erc721ABI}
-                  theme="dark"
-                  // Call the name of your smart contract function
-                  action={(contract) => contract.call("mint", [1])}
-                  onSuccess={async () => {
-                    toast.success("Successfully minted your HEX Gen 1 Bag!", {
-                      position: "top-right",
-                      autoClose: 5000,
-                      hideProgressBar: false,
-                      closeOnClick: true,
-                      pauseOnHover: true,
-                      draggable: true,
-                      progress: undefined,
-                      theme: "light",
-                    });
-                    const provider = new ethers.providers.JsonRpcProvider(
-                      process.env.NEXT_PUBLIC_ALCHEMY_LINK!
-                    );
-                    let contract = new ethers.Contract(
-                      contractAddress,
-                      erc721ABI,
-                      provider
-                    );
-
-                    let Minted = await contract.totalSupply();
-                    let total = Number(Minted);
-                    setMinted(total.toString());
-                  }}
-                  onError={(error: any) =>
-                    toast.error(error.reason, {
-                      position: "top-right",
-                      autoClose: 5000,
-                      hideProgressBar: false,
-                      closeOnClick: true,
-                      pauseOnHover: true,
-                      draggable: true,
-                      progress: undefined,
-                      theme: "light",
-                    })
-                  }
-                >
-                  Mint Now!
-                </Web3Button> */
 }
