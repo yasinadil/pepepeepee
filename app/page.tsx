35c43391a242bd15fd5f14fac9a6f60d0b1329ba
yasinadil <adilyasin205@gmail.com>
2023-05-07 21:59:53 +0500

    Final v1.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
-import Image from "next/image";
-import ConnectButton from "../public/assets/Button_Connect-Wallet.webp";
-import pepeOrig from "../public/assets/pepe_orig.webp";
-import logo from "../public/assets/Logo_pepe_pee-pee.webp";
 import Mint from "@/components/Mint/Mint";
-import bg from "../public/assets/bg.png";
 
 export default function Home() {
   return (
-    <main className=" min-h-screen flex-col items-center justify-center bg-pepe bg-[url('../public/assets/bg.png')] bg-cover bg-no-repeat">
-      <div
-        // style={{ minWidth: "260px" }}
-        className=" min-w-[260px] xl:min-w-[380px] flex flex-col min-h-screen items-center justify-center"
-      >
+    <main className=" min-h-screen flex-col items-center justify-center bg-pepe bg-[url('../public/assets/bg_mobile.jpg')] xl:bg-[url('../public/assets/bg.png')] bg-cover bg-no-repeat bg-center">
+      <div className="min-w-[260px] xl:min-w-[380px] flex flex-col min-h-screen items-center justify-center">
         <div
           id="content"
           className="flex h-[740px] flex-col items-center justify-center"
         >
-          {/* <h1 className="text-9xl">Mint your FEPES NOW!</h1> */}
-          <Mint />
+          <div className="mt-16 md:mt-0">
+            {" "}
+            <Mint />
+          </div>
         </div>
       </div>
     </main>
